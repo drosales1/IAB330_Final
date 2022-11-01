9cdfc3bf5870a6c91af5d6dfd9d72c048e7d4034
drosales1 <n10983716@qut.edu.au>
2022-11-01 12:04:01 +1000

    minor changes

--- react/src/App.js
+++ react/src/App.js
@@ -1,4 +1,4 @@
-import React, { Component, component } from 'react';
+import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Nav from './Components/nav';
 import Ward from './pages/Ward';
